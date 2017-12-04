1135e8a4d2386a6eb47ce614abfd43bfdea96e43
jaehyek <jaehyek@gmail.com>
2017-12-04 23:28:57 +0900

    backtest.py -TestStrategy_MACD 추가

--- backtest.py
+++ backtest.py
@@ -543,6 +543,254 @@ def backtracer_MovingAverage_Volume(strfilename,listcode):
 
 ## --------------------------------------------------------------
 
+
+class Strategy_MACD(bt.Strategy):
+    """
+    목적 : 일반적인 MACD을 수행한다.
+    """
+
+    params = (
+        ('printlog', False),
+        ('code', ""),
+        ('strfilename', ""),
+        ('macd1', 12),
+        ('macd2', 26),
+        ('macdsig', 9),
+    )
+
+    def log(self, txt, dt=None, doprint=False):
+        ''' Logging function fot this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        self.datavolume = self.datas[0].volume
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        macd = bt.indicators.MACDHisto(self.datas[0], period_me1=self.p.macd1,
+                                       period_me2=self.p.macd2,
+                                       period_signal=self.p.macdsig)
+
+        # Cross of macd.macd and macd.signal
+        self.macdsig = bt.ind.CrossOver(macd.macd, macd.signal)
+
+
+    def notify_order(self, order):
+        """
+        Receives an order whenever there has been a change in one
+        :param order:
+        :return:
+        """
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enougth cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        """
+        Receives a trade whenever there has been a change in one
+        :param trade:
+        :return:
+        """
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % (trade.pnl, trade.pnlcomm))
+        # pnl (float)    : current profit and loss of the trade (gross pnl)
+        # pnlcomm (float): current profit and loss of the trade minus commission (net pnl)
+
+    def prenext(self):
+        # print('prenext:: current period:',  len(self), self.datas[0].datetime.date(0))
+        pass
+
+    def nextstart(self):
+        # print('nextstart:: current period:', len(self), self.datas[0].datetime.date(0))
+        pass
+
+    def next(self):
+        """
+        This method will be called for all remaining data points when the minimum period for all datas/indicators have been meet.
+        :return:
+        """
+
+        # print('next:: current period:', len(self), self.datas[0].datetime.date(0))
+
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            if self.macdsig[0] > 0.0:
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.macdsig[0] < 0.0:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+    def stop(self):
+        try:
+            f_csv = open(self.params.strfilename, "a", encoding='utf-8')
+            historytrades = list(list(self._trades.copy().values())[0].values())[0]
+            nstake = self.getsizer().params.stake
+            listprofitrate = []
+            for dealtemp in historytrades:
+                cost = nstake * dealtemp.price
+                netprofit = dealtemp.pnlcomm
+                profitrate = netprofit / cost
+                listprofitrate.append(profitrate)
+                strcsvout = "%s,%s,%s,%s,%s,%.5f\n" % ( self.params.code, self.p.macd1, self.p.macd2,
+                    self.p.macdsig,  "rate", profitrate )
+                f_csv.write(strcsvout)
+
+            mean = np.mean(listprofitrate)
+            std = np.std(listprofitrate)
+            _, pvalue = normaltest(listprofitrate)
+            strcsvout = "%s,%s,%s,%s,%s,%.5f,%s,%.5f,%.5f,%.5f,%.5f\n"%(self.params.code, self.p.macd1, self.p.macd2,
+                        self.p.macdsig, "sum", self.broker.getvalue(), len(listprofitrate),
+                        mean, std, 1 - norm.cdf(-mean / std), pvalue  )
+            f_csv.write(strcsvout)
+            f_csv.close()
+
+            self.log('code=%s macd1=%s macd2=%s macdsig=%s  profit=%.2f' % (self.params.code, self.p.macd1, self.p.macd2,
+                        self.p.macdsig,  self.broker.getvalue()), doprint=True)
+
+        except:
+            pass
+
+        # try:
+        #
+        #     self.log('(MA Period %2d, %2d) Ending Value %.2f' % (self.params.maPeriodShort,self.params.maPeriodLong, self.broker.getvalue()), doprint=True)
+        #     historytrades = list(list(self._trades.copy().values())[0].values())[0]
+        #     nstake = self.getsizer().params.stake
+        #     listprofitrate = []
+        #     for dealtemp in historytrades:
+        #         cost = nstake * dealtemp.price
+        #         netprofit = dealtemp.pnlcomm
+        #         listprofitrate.append(netprofit / cost)
+        #     mean = np.mean(listprofitrate)
+        #     std = np.std(listprofitrate)
+        #     _, pvalue = normaltest(listprofitrate)
+        #     # 원래의 cost에 비해서 net-profit 의 rate 을 구하고,  이 rate의 평균과 표준분산을 구해서,
+        #     # 평균 얼마의 승률이 있는지 cdf을 이용해서 구한다.
+        #     # 승률이  50%이면 본전이므로,  최소한 > 50% 이어야 한다.
+        #     print("N:%s, mean:%s, std:%s, profitable rate : %s, p-value:%.2f" % (len(listprofitrate), mean, std, 1 - norm.cdf(-mean / std), pvalue))
+        #     if ( pvalue < 0.05) :
+        #         f_csv = open(self.params.strfilename, "a", encoding='utf-8')
+        #         strcsvout = "%s,%s,%s,%s,%s,%s,%s,%s,%.2f,%s,%s\n"%(self.params.code,self.params.maPeriodShort,self.params.maPeriodLong,self.broker.getvalue(),len(listprofitrate),
+        #                             mean, std, 1 - norm.cdf(-mean / std), pvalue,  self.params.maVolPeriod, self.params.VolumeMultiple )
+        #         f_csv.write(strcsvout)
+        #         f_csv.close()
+        # except:
+        #     pass
+
+def backtracer_MACD(strfilename,listcode):
+    lencode = len(listcode)
+    countleft = lencode
+
+
+    for code in listcode:
+        try :
+            print("--------------------------- %s/%s, code : %s ----------------------------"%(countleft,  lencode, code ))
+            countleft -= 1
+            df = utils.get_dataframe_with_code(code)
+            cerebro = bt.Cerebro()
+
+            cerebro.optstrategy(Strategy_MACD, code=code, strfilename=strfilename,
+                                macd1=[8,10,12,14,16,18],
+                                macd2=[20,22,24,26,28,30,32],
+                                macdsig = [6,7,8,9,10,11,12,13]
+                                )
+
+            serialdatetime = [datetime.datetime.strptime(str(bb), "%Y%m%d") for bb in df.index]
+            df = df.set_index(pd.DatetimeIndex(serialdatetime))
+            data = df[["현재가", "거래량", "시가", "고가", "저가"]]
+            data.columns = ['close', 'volume', 'open', 'high', 'low']
+
+            data = bt.feeds.PandasData(dataname=data, timeframe=1, openinterest=None)
+
+            # Add the Data Feed to Cerebro
+            cerebro.adddata(data)
+
+            # Set our desired cash start
+            # cash단위가 원으로 되므로  1억으로 확대한다.
+            cerebro.broker.setcash(100000000.0)
+
+            # Add a FixedSize sizer according to the stake
+            cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+            # Set the commission
+            # 매도거래세 : 0.3%,  매도매수수수료:0.165%  -> total : 0.33%
+            # 매수 매도에 둘로 나누면, 0.165%가 된다.
+            cerebro.broker.setcommission(commission=0.00165)
+
+            # Print out the starting conditions
+            print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+            # Run over everything
+            listliststrategy = cerebro.run()
+
+            # Print out the final result
+            print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        except:
+            f = open("error.log", "a", encoding='utf-8')
+            f.write("error code is %s\n"%(code))
+            f.close()
+            pass
+
+
+    print("Elapse time is %s "%(datetime.datetime.now() - timeprev ))
+    # Plot the result
+    # cerebro.plot()
+
+## --------------------------------------------------------------
+
 if __name__ == '__main__':
     cmdlineopt = argparse.ArgumentParser(description='filter row data from csv file ')
     cmdlineopt.add_argument('-m', action="store", dest="backtest_method", default='averagevolume', help='select the backtest method')
@@ -569,6 +817,8 @@ if __name__ == '__main__':
         backtracer_MovingAverage(strfilename,listcode )
     elif clsvar.backtest_method == 'averagevolume' :
         backtracer_MovingAverage_Volume(strfilename,listcode )
+    elif clsvar.backtest_method == 'macd' :
+        backtracer_MACD(strfilename,listcode )
 
     print("Elapse time is %s " % (datetime.datetime.now() - timeprev))
 
